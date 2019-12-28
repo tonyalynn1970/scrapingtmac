7c1d2f6835b50b909f1584c7b47a1ae40e9e8a6d
tonyalynn1970 <55565993+tonyalynn1970@users.noreply.github.com>
2019-12-27 20:59:50 -0500

    Adding models working on server.js and app.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scrapingtmac
 Scraping New Articles
+The News Scraper Application scrapes articles from the Hacker News homepage and displays the headline, image, and summary on the application's home page. If the user clicks on the article headline, the original article will open in a separate window. If the "Scrape New Articles" button is pressed, the application will scrape any new articles that have been published on the Hacker News site, and delete any old unsaved articles that are no longer featured.
+
+The application can also be accessed via the deployed Heroku page:
--- server.js
+++ server.js
@@ -1,22 +1,59 @@
 const cheerio = require('cheerio');
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const mongoose = require('mongoose');
+const app = express();
 
-axios.get("https://news.ycombinator.com/").then((response) => {
-    // console.log(response.data)
-    // const $ = cheerio.load(html);
+app.use(express.static('public'));
 
-    const $ = cheerio.load(response.data);
-    const output = $('.storylink')
-    for (let i = 0; i < output.length; i++) {
-        // console.log(output[i])
-        var linktext = $(output[i]).text()
-        console.log(linktext)
-        results.push({
-            title: title,
-            link: link
-        })
-    }
+mongoose.connect("mongodb://localhost/Scrapers", { useNewUrlParser: true });
+app.get("/scrape", function (req, res) {
+
+    axios.get("https://news.ycombinator.com/").then((response) => {
+
+        const results = [];
+
+        const $ = cheerio.load(response.data);
+        const output = $('.storylink')
+        for (let i = 0; i < output.length; i++) {
+
+            var title = $(output[i]).text()
+            const link = $(output[i]).attr("href")
+
+            if (title && link) {
+                results.push({
+                    _id: "",
+                    title: title,
+                    link: link
+                })
+                var r = {
+                    title: title,
+                    link: link
+                }
+                db.Article.create(r)
+                    .then(function (dbArticle) {
+
+                        console.log(dbArticle);
+                    })
+                    .catch(function (err) {
+                        // If an error occurred, log it
+                        console.log(err);
+                    });
+
+            };
+
+        }
+        console.log(results)
+        res.json(results)
+    })
+})
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+app.listen(3000, function () {
+    console.log("App running on port 3000");
 
 });
+
